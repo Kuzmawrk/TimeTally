8d1aea76c02f744caee4e74c3786887a3ba1aa6f
Кузьма Кузьмин <kuzmakuzmin@MacBook-Air-Kuzma.local>
2025-05-11 17:24:55 +0300

    Undefined symbol: type metadata accessor for Extension.AppDelegate
    Undefined symbol: nominal type descriptor for Extension.AppDelegate
    
    Undefined symbol: Extension.RemoteScreen.init(content: () -> A) -> Extension.RemoteScreen<A>
    
    Undefined symbol: nominal type descriptor for Extension.RemoteScreen
    
    Undefined symbol: protocol conformance descriptor for Extension.RemoteScreen<A> : SwiftUI.View in Extension
    
    Linker command failed with exit code 1 (use -v to see invocation)

--- TimeTally/TimeTallyApp.swift
+++ TimeTally/TimeTallyApp.swift
@@ -1,17 +1,13 @@
 import SwiftUI
-import Extension
 
 @main
 struct TimeTallyApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var themeManager = ThemeManager()
     
     var body: some Scene {
         WindowGroup {
-            RemoteScreen {
-                ContentView()
-                    .environmentObject(themeManager)
-            }
+            ContentView()
+                .environmentObject(themeManager)
         }
     }
 }
